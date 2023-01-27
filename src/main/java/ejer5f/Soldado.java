9c793883ecceba803f9a0149c2f8ed8f10b9d980
Noelia <perezgonzaleznoelia08@gmail.com>
2023-01-27 12:59:29 +0100

    270123

---
+++ src/main/java/ejer5f/Soldado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejer5f;
+
+/**
+ *
+ * @author noelia
+ */
+public class Soldado {
+
+    private String nif;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    
+}
